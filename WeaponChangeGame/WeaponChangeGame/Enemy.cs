a4465b24fc79dbb1b1bf11a11c4fdbf0f9c06a79
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-08-31 09:31:20 +0900

    [Feat] Enemy 상위 추상클래스 추가

--- WeaponChangeGame/WeaponChangeGame/Enemy.cs
+++ WeaponChangeGame/WeaponChangeGame/Enemy.cs
@@ -1,6 +1,15 @@
 namespace WeaponChangeGame;
 
-public class Enemy
+public abstract class Enemy
 {
-    
+    public int Hp { get; private set; }
+    public int Damage { get; private set; }
+    public string Name { get; private set; }
+
+    public Enemy(string name, int hp, int damage)
+    {
+        Name = name;
+        Hp = hp;
+        Damage = damage;
+    }
 }
