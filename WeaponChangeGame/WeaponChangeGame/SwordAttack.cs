8b803d376f8f3c2650e53de3e08f233d3dedb83a
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-08-31 17:44:39 +0900

    [Feat] 검술 공격전략 클래스 추가

--- WeaponChangeGame/WeaponChangeGame/SwordAttack.cs
+++ WeaponChangeGame/WeaponChangeGame/SwordAttack.cs
@@ -1,6 +1,14 @@
 namespace WeaponChangeGame;
 
-public class SwordAttack
+public class SwordAttack : IAttackStrategy
 {
-    
+    public int CalculateDamage(int damage, ArmorType armorType)
+    {
+        return 0;
+    }
+
+    public string AttackMessage()
+    {
+        return "";
+    }
 }
