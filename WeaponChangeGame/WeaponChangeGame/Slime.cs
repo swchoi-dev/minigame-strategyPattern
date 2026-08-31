fdd801dfb5c71c6dce51cb41143db78e2e229b99
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-08-31 17:44:14 +0900

    [Feat] 슬라임 클래스 공격 메서드 인수값 수정

--- WeaponChangeGame/WeaponChangeGame/Slime.cs
+++ WeaponChangeGame/WeaponChangeGame/Slime.cs
@@ -2,20 +2,29 @@ namespace WeaponChangeGame;
 
 public class Slime : Enemy, IAttack, ITakeDamage
 {
-    public Slime(string name, int hp, int damage) : base(name, hp, damage)
+    public ArmorType ArmorType { get; }
+    
+    public Slime(string name, int hp, int damage, ArmorType armor) : base(name, hp, damage)
     {
-        
+        ArmorType = armor;
     }
 
-    public void Attack()
+    public void Attack(ITakeDamage player)
     {
         Console.WriteLine($"{Name}이 강력하게 몸을 부딪혔다..!");
         Console.WriteLine($"{Damage}만큼의 피해를 입혔다..!");
+        player.TakeDamage(Damage);
     }
 
     public void TakeDamage(int damage)
     {
-        Hp -= damage;
         Console.WriteLine($"{Name}은 {damage}의 데미지를 입었다..!");
+        Hp -= damage;
+    }
+
+    public override void Info()
+    {
+        Console.WriteLine($"{Name}은 {ArmorType}타입 입니다.");
+        Console.WriteLine($"{Name}은 물리 공격에 데미지가 90% 반감됩니다.");
     }
 }
