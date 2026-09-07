d6668b6c6a0585f4fd736fda899c5b94a704e548
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-09-07 10:19:06 +0900

    [Feat] 게임 메뉴선택 로직 추가

--- WeaponChangeGame/WeaponChangeGame/Game.cs
+++ WeaponChangeGame/WeaponChangeGame/Game.cs
@@ -5,40 +5,51 @@ public class Game
     public void Run()
     {
         bool isPlaying = true;
-        SwordAttack swordAttack = new SwordAttack();
-        Player player = new Player("Dile", 10, 300, swordAttack);
-        Slime slime = new Slime("파랑 슬라임", 100, 30, ArmorType.물리내성);
+        bool enemiesDead = false;
+        int stageIndex = 0;
+        int strategyIndex = 0;
+        IAttackStrategy[] strategies =
+        {
+            new SwordAttack(),
+            new BowAttack(),
+            new MagicAttack()
+        };
+        Enemy[] enemies =
+        {
+            new Slime("파랑 슬라임", 100, 30, ArmorType.물리내성)
+        };
+        Player player = new Player("Dile", 10, 300, strategies[strategyIndex]);
         
         while (isPlaying)
         {
             Console.Clear();
-            PrintMenu(player, slime);
+            PrintMenu(player, enemies[stageIndex]);
+            Console.WriteLine();
             
-            // PrintKioskMain(itemList);
-            //
-            // var picked = (KioskMenu)ConsoleInput.ReadIntInRange("번호 : ", 1, 4);
-            // switch (picked)
-            // {
-            //     case KioskMenu.담기:
-            //         // 메뉴번호와 수량을 묻기
-            //         OrderItem(itemList);
-            //         break;
-            //     case KioskMenu.전체비우기:
-            //         // 장바구니 통째로 비우기
-            //         ClearShoppingCart(itemList);
-            //         break;
-            //     case KioskMenu.결제:
-            //         // 합계 금액 출력, 받은 금액 묻기
-            //         PayShoppingCart();
-            //         ClearShoppingCart(itemList);
-            //         break;
-            //     case KioskMenu.영업종료:
-            //         // 그날의 총 주문건수와 총 매출액 출력
-            //         isStoreOpen = false;
-            //         CloseStore();
-            //         break;
-            // }
-            if (player.IsDead || slime.IsDead)
+            Console.WriteLine("1. 공격");
+            Console.WriteLine("2. 무기 변경");
+            Console.WriteLine("3. 상태 확인");
+            var picked = (GameMenu)ConsoleInput.ReadIntInRange("> ", 1, 3);
+            switch (picked)
+            {
+                case GameMenu.공격:
+                    break;
+                case GameMenu.무기변경:
+                    break;
+                case GameMenu.몬스터정보:
+                    enemies[stageIndex].Info();
+                    break;
+            }
+
+            foreach (Enemy enemy in enemies)
+            {
+                if (enemy.IsAlive)
+                {
+                    enemiesDead = false;
+                }
+            }
+            
+            if (player.IsDead || enemiesDead)
             {
                 isPlaying = false;
                 Console.WriteLine("========== END ==========");
@@ -51,6 +62,13 @@ public class Game
     {
         Console.WriteLine("========= 전략 패턴 전투 =========");
         Console.WriteLine($"{player.Name} HP: {player.Hp} / {enemy.Name} HP: {enemy.Hp}");
-        Console.WriteLine($"현재 전술: {player.AttackType}");
+        Console.WriteLine($"현재 전술: [{player.AttackType}]");
+    }
+
+    public enum GameMenu
+    {
+        공격 = 1,
+        무기변경,
+        몬스터정보
     }
 }
