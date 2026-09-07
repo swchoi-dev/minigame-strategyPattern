e2925f091f8c8de8398d46e3836e97700a4d7009
swchoi-dev <choi.sungwon.179.70@gmail.com>
2026-09-07 10:18:54 +0900

    [Feat] 원거리, 마법 공격전략 추가

--- WeaponChangeGame/WeaponChangeGame/BowAttack.cs
+++ WeaponChangeGame/WeaponChangeGame/BowAttack.cs
@@ -1,6 +1,16 @@
 namespace WeaponChangeGame;
 
-public class BowAttack
+public class BowAttack : IAttackStrategy
 {
+    public StrategyName Name => StrategyName.원거리공격;
     
+    public int CalculateDamage(int damage, ArmorType armorType)
+    {
+        return 0;
+    }
+
+    public string AttackMessage()
+    {
+        return "활로 원거리공격..!";
+    }
 }
--- WeaponChangeGame/WeaponChangeGame/MagicAttack.cs
+++ WeaponChangeGame/WeaponChangeGame/MagicAttack.cs
@@ -1,6 +1,16 @@
 namespace WeaponChangeGame;
 
-public class MagicAttack
+public class MagicAttack : IAttackStrategy
 {
+    public StrategyName Name => StrategyName.마법공격;
     
+    public int CalculateDamage(int damage, ArmorType armorType)
+    {
+        return 0;
+    }
+
+    public string AttackMessage()
+    {
+        return "마법으로 공격..!";
+    }
 }
